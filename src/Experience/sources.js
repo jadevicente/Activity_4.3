716da3fa4a70ee184589f7d20421c3e2824e3c7d
jadevicente <jadevicente96@gmail.com>
2025-11-09 02:55:15 +0800

    Fix package.json and vite

--- src/Experience/sources.js
+++ src/Experience/sources.js
@@ -4,27 +4,27 @@ export default [
         type: 'cubeTexture',
         path:
         [
-            'textures/environmentMap/px.jpg',
-            'textures/environmentMap/nx.jpg',
-            'textures/environmentMap/py.jpg',
-            'textures/environmentMap/ny.jpg',
-            'textures/environmentMap/pz.jpg',
-            'textures/environmentMap/nz.jpg'
+            './textures/environmentMap/px.jpg',
+            './textures/environmentMap/nx.jpg',
+            './textures/environmentMap/py.jpg',
+            './textures/environmentMap/ny.jpg',
+            './textures/environmentMap/pz.jpg',
+            './textures/environmentMap/nz.jpg'
         ]
     },
     {
         name: 'grassColorTexture',
         type: 'texture',
-        path: 'textures/dirt/color.jpg'
+        path: './textures/dirt/color.jpg'
     },
     {
         name: 'grassNormalTexture',
         type: 'texture',
-        path: 'textures/dirt/normal.jpg'
+        path: './textures/dirt/normal.jpg'
     },
     {
         name: 'foxModel',
         type: 'gltfModel',
-        path: 'models/Fox/glTF/Fox.gltf'
+        path: './models/Fox/glTF/Fox.gltf'
     }
 ] 
